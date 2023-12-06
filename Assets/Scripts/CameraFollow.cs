22d40a8e3b9022741880817033dbb183519340a8
Thomaspls-unity <152604731+Thomaspls-unity@users.noreply.github.com>
2023-12-06 15:56:49 +0000

    Challenge 2.3 Switching Cams
    -Setup 3 difference cams
    -Options in code to switch using 3 different keys
    -Option to switch using single key, customisable in the inspector

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,6 +7,9 @@ public class CameraFollow : MonoBehaviour
     public Transform vehicle; //reference to the vehicle
     private Vector3 offset; //distance between cam and vehicle
     public float smoothSpeed = 0.1f;
+    public bool isThirdPersonCam = false;
+    //public Vector3 thirdPerson = new Vector3(0, 0, 0);
+    //public Vector3 firstPerson = new Vector3(0, 0, 0);
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +19,15 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //if (Input.GetKey(KeyCode.F)) This test to change the camera position didn't work, moved to a seperate script
+        //{
+        //    transform.position = firstPerson + vehicle.position + offset;
+        //}
 
+        //if (Input.GetKey(KeyCode.T))
+        //{
+        //    transform.position = thirdPerson + vehicle.position + offset;
+        //}
     }
 
     private void LateUpdate() //Last thing to be called on a frame
@@ -37,6 +48,10 @@ public class CameraFollow : MonoBehaviour
 
         transform.position = smoothPosition;
 
-        transform.LookAt(vehicle);
+        if (isThirdPersonCam == true)
+        {
+            transform.LookAt(vehicle);
+        }
+        
     }
 }
---
+++ Assets/Scripts/CameraToggle.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraToggle : MonoBehaviour
+{
+    public Camera thirdPerson;
+    public Camera firstPerson;
+    public Camera sideView;
+    public KeyCode keyCode;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thirdPerson.enabled = true;
+        firstPerson.enabled = false;
+        sideView.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SwitchCamsUsingOneKey();
+        //SwitchCamsUsingThreeKeys();
+    }
+
+    private void SwitchCamsUsingOneKey()//This method only works with 2 options
+                                        //With 2, when 1 is disabled the other is enabled.
+                                        //With 3, when 1 is disabled, the other 2 would be enabled.
+    {
+        //if (Input.GetKeyDown(KeyCode.C))
+        //{
+        //    thirdPerson.enabled = !thirdPerson.enabled;
+        //    sideView.enabled = !sideView.enabled;
+        //    firstPerson.enabled = !firstPerson.enabled;
+        //}
+        if (Input.GetKeyDown(keyCode))
+        {
+        if (thirdPerson.enabled == true)
+            {
+                thirdPerson.enabled = false;
+                firstPerson.enabled = true;
+                sideView.enabled = false;
+            }else
+                if (firstPerson.enabled == true)
+            {
+                thirdPerson.enabled = false;
+                firstPerson.enabled = false;
+                sideView.enabled = true;
+            }else
+                if (sideView.enabled == true)
+            {
+                thirdPerson.enabled = true;
+                firstPerson.enabled = false;
+                sideView.enabled = false;
+            }
+        }
+    }
+
+    private void SwitchCamsUsingThreeKeys()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            SwitchToFirstPerson();
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            SwichToThirdPerson();
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SwitchToSideView();
+        }
+    }
+
+    void SwitchToFirstPerson()
+    {
+        firstPerson.enabled = true;
+        thirdPerson.enabled = false;
+        sideView.enabled = false;
+    }
+
+    void SwichToThirdPerson()
+    {
+        thirdPerson.enabled = true;
+        firstPerson.enabled = false;
+        sideView.enabled = false;
+    }
+
+    void SwitchToSideView()
+    { 
+        sideView.enabled = true;
+        firstPerson.enabled = false;
+        thirdPerson.enabled = false;
+    }
+}
