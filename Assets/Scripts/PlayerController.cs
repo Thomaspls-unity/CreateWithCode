ea509e8e377b8c9b6ede494cd9038c63d098fc11
Thomaspls-unity <152604731+Thomaspls-unity@users.noreply.github.com>
2023-12-07 16:28:07 +0000

    End of day 2.4
    -Added physics based movement
    -Added a trigger for falling off the map
    -Can drive through buildings still
    -Not sure how to turn off inputs in trigger

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20f;
-    
+    public float horizontalInput;
+    public float verticalInput;
+    public float speed = 20.0f;
+    public float defaultSpeed = 20.0f;
+    public float sprintSpeed = 40.0f;
+    public float rotateSpeed = 60.0f;
+    public Rigidbody playerRb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        GetInput();
+        SpeedBoost();
     }
 
-    
+    public void GetInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        MoveCarWithForceAndTorque();
+        MoveCarWithPhysicsRotation();
+    }
+
+    private void MoveCarWithForceAndTorque()
+    {
+        if (verticalInput > 0.01f || verticalInput < -0.01f)
+        {
+            playerRb.AddForce(transform.forward * verticalInput * speed);
+        }
+
+        if (horizontalInput > 0.01f || horizontalInput < -0.01f)
+        {
+            playerRb.AddTorque(transform.up * horizontalInput * rotateSpeed);
+        }
+    }
+
+    private void MoveCarWithPhysicsRotation()
+    {
+        //Vector3 moveDirection = new Vector2(horizontalInput, verticalInput);
+        Vector3 moveDirection = transform.forward * verticalInput;
+        playerRb.MovePosition(playerRb.position + moveDirection * speed * Time.fixedDeltaTime);
+
+        if (horizontalInput > 0.01f || horizontalInput < -0.01f)
+        {
+            float turnSpeed = horizontalInput * rotateSpeed * Time.fixedDeltaTime;
+            Quaternion turnRotation = Quaternion.Euler(0f, turnSpeed, 0f);
+            playerRb.MoveRotation(playerRb.rotation * turnRotation);
+        }
+    }
+
+    public void SpeedBoost()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = sprintSpeed;
+        }
+        else
+        {
+            speed = defaultSpeed;
+        }
+    }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Transform player;
+
     public float speed = 2.0f;
+    public float defaultSpeed = 2.0f;
+    public float sprintSpeed = 4.0f;
     public float rotateSpeed = 30.0f;
+
     private float horizontalInput;
     private float verticalInput;
+
+    private bool speedInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,8 +26,9 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         GetInput();
+        SpeedBoost();
         //MovePlayerWithTranslate(transform, speed); //Function / Method, they are the same thing
-        MovePlayerWithRotate(transform, rotateSpeed, speed);
+        //MovePlayerWithRotate(transform, rotateSpeed, speed);
     }
 
     private void MovePlayerWithTranslate(Transform player, float speed = 10.0f) //Function parameter
@@ -31,7 +39,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void MovePlayerWithRotate(Transform player, float rotateSpeed = 15.0f, float speed = 10.0f) //Function parameter
     {
-        player.Translate(player.forward * speed * verticalInput * Time.deltaTime);
+        player.Translate(Vector3.forward * speed * verticalInput * Time.deltaTime);
         if (verticalInput > 0.01f || verticalInput < -0.01f)
         {
             player.Rotate(Vector3.up, rotateSpeed * horizontalInput * Time.deltaTime);
@@ -44,5 +52,16 @@ public class PlayerMovement : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
     }
 
-
+    private void SpeedBoost()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            //player.Translate(player.forward * speed * 2 * verticalInput * Time.deltaTime);
+            speed = sprintSpeed;
+        }
+        else
+        {
+            speed = defaultSpeed;
+        }
+    }
 }
